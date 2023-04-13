a8dd08c311c7601ac16f955710b72ef53fa1427f
Culturology's Undead <a.pawluczenko@gmail.com>
2023-04-13 21:42:12 +0300

    Demo 1 passed

--- draft.R
+++ draft.R
@@ -5,3 +5,18 @@ women2 <- women
 women2$height[4] <- 155
 women2$weight[4] <- 56
 women2
+
+install.packages("hexSticker")
+library(hexSticker)
+p <- visualize_pps(swiss)
+s <- sticker(
+  p + guides(fill = 'none') + theme(text = element_blank()),
+  package = "ppsr",
+  h_color = '#'
+  # p_size = 20,
+  # s_x = .8,
+  # s_y = .6,
+  # s_width = 1.4,
+  # s_height = 1.2,
+  filename = "images/ppsr_plot.png"
+)
