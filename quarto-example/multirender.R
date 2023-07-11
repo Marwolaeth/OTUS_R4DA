587bdf8ea5a36d2459ce7a6827e880ae121fc6b9
Culturology's Undead <a.pawluczenko@gmail.com>
2023-07-11 19:20:14 +0300

    New multiplex & more examples

---
+++ quarto-example/multirender.R
@@ -0,0 +1,18 @@
+library(arrow)
+library(dplyr)
+
+(jobs <- open_dataset('data/db/meta') |>
+    distinct(job) |>
+    pull(as_vector = TRUE)
+)
+
+render_job_report <- function(job){
+  quarto::quarto_render(
+    input = "salaries.qmd",
+    output_format = 'html',
+    execute_params = list(job = job),
+    output_file = glue::glue("{job}-report.html")
+  )
+}
+
+purrr::walk(jobs, render_job_report)
